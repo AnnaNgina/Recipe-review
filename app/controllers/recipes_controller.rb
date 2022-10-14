4dbeb0dd3756e9108c932ea474eee6fa0b945bbe
annastacia <annastaciangina96@gmail.com>
2022-10-14 14:13:32 +0300

    added recipe controller function

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,36 +1,57 @@
-class RecipesController < ApplicationController
+lass RecipesController < ApplicationController
+    protect_from_forgery prepend: true
 
     def index
-     recipes =Recipe.all
      render json: Recipe.all, status: :ok
-    end    
+    end   
 
     def show
-      recipe = Recipe.find_by(id: params[:id])
-    if recipe
-        render json: recipe, status: :ok
-    else
-        render json: {error: "Recipe not found"}, status: :not_found
-    end
-end
-
-    def update
         recipe = Recipe.find_by(id: params[:id])
-        if recipe
-            render json: { errors: invalid.errors.full_messages }, status: :unprocessable_entity
-        else
-            render json: {error: "Recipe not found"}, status: :not_found
-        end
+      if recipe
+          render json: recipe, status: :ok
+      else
+          render json: { error: "Review not found" }, status: :not_found
+      end
     end
 
-#     def destroy 
-#         recipe = Recipe.find_by(id: params[:id]) 
-    
-#         if recipe.destroy 
-#         head :no_content
-    
-#          else 
-#             render json: { error: recipe.errors.messages }, status: 422
-#         end 
-#         end
+    def create
+        recipe = Recipe.create(recipe_params)
+       
+
+        #  if recipe.save
+        render json: recipe, status: :created
+   
+
+    #   else
+    #     render json: { error: recipe.errors.messages }, status 422 
+    #     end
+    end   
+    def update
+        recipe = Recipe.find_by(id:params[:id])
+
+        if recipe
+            recipe.update(recipe_params)
+            render json:recipe ,status: :accepted
+
+        else
+            
+            render json:{error: "Recipe not found"}, status: :not_found
+        end
+    end        
+    def destroy
+        recipe = Recipe.find_by(id:params[:id])
+
+        if recipe
+            recipe.destroy
+            head :no_content
+        else
+            render json {error:"Recipe not found"}, status: :not_found
+        end
+    end            
+
+    private
+    def recipe_params
+        params.permit(:name, :image_url, :description)
+    end    
 end
+
